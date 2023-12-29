a32b10f2df50866629a521c2d772f2b6bcfdf4a1
sweety11111 <124230712+sweety11111@users.noreply.github.com>
2023-12-29 12:10:42 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,14 @@
 import streamlit as st
 st.set_page_config(page_title='cats')
-st.markdown("Types of cats")
-st.image("./cat1.jpeg")
-st.write("- Persian Cat")
-st.image("./cat2.jpeg",width=500)
-st.write("- Ragdoll Cat")
+st.header("Types of cats
+
+col1,col2=st.columns(2)
+with col1:
+  st.subheader("Persian Cat")
+  st.image("./cat1.jpeg")
+  st.write("Persian Cats are cute")
+with col2:
+  st.subheader("Ragdoll Cat")
+  st.image("./cat2.jpeg",width=500)
+  st.write("Ragdoll Cats are proud")
+
