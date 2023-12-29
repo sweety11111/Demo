2db54397714ac14c02e34b01b88d154ce35f6b8e
sweety11111 <124230712+sweety11111@users.noreply.github.com>
2023-12-29 11:00:14 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,4 +2,5 @@ import streamlit as st
 st.set_page_config(page_title='cats')
 st.markdown("## Types of cats")
 st.write("- Persian cat")
+st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 st.write("- White cat")
